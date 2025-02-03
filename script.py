3dc147c01bb28f4676dbda60861f148b6caf7676
RoboticWolf <robotics@roboticwolf.co.za>
2025-02-03 15:12:54 +0200

    Default shipping type set to 'overnight'

--- script.py
+++ script.py
@@ -2,7 +2,7 @@ from nile import get_distance, format_price, SHIPPING_PRICES
 from test import test_function
 
 # Define calculate_shipping_cost() here:
-def calculate_shipping_cost(from_coords, to_coords, shipping_type):
+def calculate_shipping_cost(from_coords, to_coords, shipping_type = 'Overnight'):
     from_lat, from_long = from_coords
     to_lat, to_long = to_coords
     distance = get_distance(from_lat, from_long, to_lat, to_long)
